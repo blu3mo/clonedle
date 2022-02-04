d3025f9166a87ef2ca9152954e3354a493341504
bluemountain <shutaro.aoyama@gmail.com>
2022-02-04 18:17:04 +0900

    Wordle Clone

--- src/App.js
+++ src/App.js
@@ -1,74 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+const random = (max) => {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
+};
+
+const checkCharMatch = (guess, answer) => {
+  let guessArray = guess.split("")
+  let answerArray = answer.split("")
+  return guessArray.map((char, i) => {
+    if (answerArray[i] === char) {
+      return "eat"
+    } else if (answerArray.includes(char)) {
+      return "bite"
+    } else {
+      return ""
+    }
+  })
 }
 
-function App() {
+function GuessInput({ onGuess }) {
   const [val, setVal] = useState("");
 
-  const maxTrialCount = 5
-  const [trials, setTrials] = useState([]);
+  const handleChange = e => setVal(e.target.value);
 
-  const generateAnswer = () => {
-    console.log("gen")
-    return Math.floor(Math.random() * 49) + 1;
-  }
-  const [answer, setAnswer] = useState(generateAnswer);
-
-  const [isGameCleared, setIsGameCleared] = useState(false);
-
-  const handleChange = e => setVal(e.target.value)
-  const handleTrial = e => {
-    if (maxTrialCount-trials.length > 0 && !isGameCleared) {
-      console.log(parseInt(val) === answer)
-      if (parseInt(val) === answer) {
-        setIsGameCleared(true)
-      } else {
-        setVal("")
-        setTrials([...trials, val])
-      }
+  const handleKeyDown = e => {
+    if (e.keyCode === 13) {
+      onGuess(val)
+      setVal("")
     }
-    console.log(answer)
-  }
-
-  const resetTrial = () => {
-    setTrials([])
-    setVal("")
-    setAnswer(generateAnswer())
-    setIsGameCleared(false)
   }
 
   return (
-    <>
-      <p>Wordle</p>
+    <div id="form">
       <input
+        id="input"
+        type="text"
         value={val}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
-      <input
-        type="button"
-        value="予想する"
-        onClick={handleTrial}
-      />
-      <p>
-        {(isGameCleared) && "clear!"}
-      </p>
-      <p>
-        { (trials.length > 0 && !isGameCleared) && ((answer > trials[trials.length - 1]) ? "もっと大きい" : "もっと小さい" )}
-      </p>
-      <p>
-        あと{maxTrialCount-trials.length}回
-      </p>
-      <input
-        type="button"
-        value="はじめから"
-        onClick={resetTrial}
-      />
-    </>
+      {/*<button type="submit" onClick={handleClick}>予想する</button>*/}
+    </div>
+  );
+}
+
+function Guess({children, match}) {
+  let i = 0
+
+  return (
+    <div>
+      {[...children].map((char, i) => {
+        return <p
+            className={"guessChar " + match[i]}
+            key={i}
+        >
+            {char}
+        </p>
+        i++;
+      })}
+    </div>
   )
+}
+
+function App() {
+  const max = 50;
+  const initialCount = 6;
+  const [answer, setAnswer] = useState("apple");
+  const [count, setCount] = useState(initialCount);
+  const [message, setMessage] = useState('');
+  const [guesses, setGuesses] = useState([]);
+
+  const judge = guess => {
+    if (count === 0) return;
+
+    setCount(count - 1);
+
+    if (guess === answer) {
+      setMessage('正解です！');
+    } else if (count === 1) {
+      setMessage('残念でした！ 正解は' + answer);
+    }
+
+    setGuesses([...guesses, guess]);
+    console.log(guesses)
+  };
+
+  const replay = () => {
+    setAnswer(random(max));
+    setCount(initialCount);
+    setMessage('');
+  };
+
+  let i = 0
+  return (
+    <>
+      {/*<p>{message}</p>*/}
+      <p>あと{count}回</p>
+      {/*<p>*/}
+      {/*  <button onClick={replay}>はじめから</button>*/}
+      {/*</p>*/}
+      {guesses.map(guess => {
+        console.log(guess)
+        i++
+        return <Guess
+          key={i}
+          match={checkCharMatch(guess, answer)}
+        >
+          {guess}
+        </Guess>
+      })}
+      <GuessInput onGuess={judge} />
+    </>
+  );
 }
 
 export default App;
