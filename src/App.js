2f2ed41b8acafa65ae8b0484c9469450aeec5097
bluemountain <shutaro.aoyama@gmail.com>
2022-02-04 15:13:26 +0900

    Number Guesser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function App() {
+  const [val, setVal] = useState("");
+
+  const maxTrialCount = 5
+  const [trials, setTrials] = useState([]);
+
+  const generateAnswer = () => {
+    console.log("gen")
+    return Math.floor(Math.random() * 49) + 1;
+  }
+  const [answer, setAnswer] = useState(generateAnswer);
+
+  const [isGameCleared, setIsGameCleared] = useState(false);
+
+  const handleChange = e => setVal(e.target.value)
+  const handleTrial = e => {
+    if (maxTrialCount-trials.length > 0 && !isGameCleared) {
+      console.log(parseInt(val) === answer)
+      if (parseInt(val) === answer) {
+        setIsGameCleared(true)
+      } else {
+        setVal("")
+        setTrials([...trials, val])
+      }
+    }
+    console.log(answer)
+  }
+
+  const resetTrial = () => {
+    setTrials([])
+    setVal("")
+    setAnswer(generateAnswer())
+    setIsGameCleared(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>Wordle</p>
+      <input
+        value={val}
+        onChange={handleChange}
+      />
+      <input
+        type="button"
+        value="予想する"
+        onClick={handleTrial}
+      />
+      <p>
+        {(isGameCleared) && "clear!"}
+      </p>
+      <p>
+        { (trials.length > 0 && !isGameCleared) && ((answer > trials[trials.length - 1]) ? "もっと大きい" : "もっと小さい" )}
+      </p>
+      <p>
+        あと{maxTrialCount-trials.length}回
+      </p>
+      <input
+        type="button"
+        value="はじめから"
+        onClick={resetTrial}
+      />
+    </>
+  )
 }
 
 export default App;
